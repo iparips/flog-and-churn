e896cca493a62ca50ebbe6b31e616c576812786a
Ilya Paripsa <ilya.paripsa@rea-group.com>
2015-11-18 08:09:35 +1100

    Extract rake task code into a class

--- lib/flog_and_churn.rb
+++ lib/flog_and_churn.rb
@@ -1,5 +1,20 @@
-require "flog_and_churn/version"
+require 'rake'
+require 'json'
+require 'flog_and_churn/flogger'
+require 'flog_and_churn/churner'
+require 'flog_and_churn/scores_builder'
 
-module FlogAndChurn
-  # Your code goes here...
+class FlogAndChurnRunner
+
+  def self.run
+    flog = FlogAndChurn::Flogger.new.score_files
+    churn = FlogAndChurn::Churner.new.score_files
+    scores = FlogAndChurn::ScoresBuilder.combine(flog, churn)
+    puts scores.top_ten
+    FileUtils.rm_rf 'reports'
+    FileUtils.mkdir 'reports'
+    FileUtils.cp_r Dir['lib/metrics/assets/*'], 'reports/'
+    File.write('reports/data.js', "onPageLoad(#{scores.top_ten.to_json});")
+  end
+
 end
--- lib/tasks/flog_and_churn.rake
+++ lib/tasks/flog_and_churn.rake
@@ -1,18 +1,8 @@
 require 'json'
-require 'churn/calculator'
-require 'flog_and_churn/flogger'
-require 'flog_and_churn/churner'
-require 'flog_and_churn/scores_builder'
+require 'flog_and_churn'
 
 desc 'flog and churn, only works when git is on master'
 task :flog_and_churn do
-  flog = Flogger.new.score_files
-  churn = Churner.new.score_files
-  scores = ScoresBuilder.combine(flog, churn)
-  puts scores.top_ten
-  rm_rf 'reports'
-  mkdir 'reports'
-  cp_r Dir['lib/metrics/assets/*'], 'reports/'
-  File.write('reports/data.js', "onPageLoad(#{scores.top_ten.to_json});")
+  FlogAndChurnRunner.run
 end
 
