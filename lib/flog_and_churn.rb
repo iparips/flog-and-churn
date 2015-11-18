55daac25b656b34d00fd516f4a01222daadd349f
Ilya Paripsa <ilya.paripsa@rea-group.com>
2015-11-18 14:19:50 +1100

    Move assets out of lib

--- lib/flog_and_churn.rb
+++ lib/flog_and_churn.rb
@@ -6,15 +6,32 @@ require 'flog_and_churn/scores_builder'
 
 class FlogAndChurnRunner
 
-  def self.run
-    flog = FlogAndChurn::Flogger.new.score_files
-    churn = FlogAndChurn::Churner.new.score_files
-    scores = FlogAndChurn::ScoresBuilder.combine(flog, churn)
-    puts scores.top_ten
-    FileUtils.rm_rf 'reports'
-    FileUtils.mkdir 'reports'
-    FileUtils.cp_r Dir['lib/metrics/assets/*'], 'reports/'
-    File.write('reports/data.js', "onPageLoad(#{scores.top_ten.to_json});")
+  class << self
+
+    def run
+      flog = FlogAndChurn::Flogger.new.score_files
+      churn = FlogAndChurn::Churner.new.score_files
+      scores = FlogAndChurn::ScoresBuilder.combine(flog, churn)
+
+      puts scores.top_ten
+
+      FileUtils.rm_rf 'reports'
+      FileUtils.mkdir 'reports'
+
+      copy_assets
+      write_scores(scores)
+    end
+
+    private
+
+    def copy_assets
+      FileUtils.cp_r Dir["#{File.dirname(__FILE__)}/../assets/*"], 'reports/'
+    end
+
+    def write_scores(scores)
+      File.write('reports/data.js', "onPageLoad(#{scores.top_ten.to_json});")
+    end
+
   end
 
 end
